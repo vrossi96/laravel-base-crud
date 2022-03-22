901e27ec1188093172469662299e57d7905a7284
vrossi96 <vrossi396@yahoo.it>
2022-03-22 14:33:39 +0100

    add main layout

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-   <title>COMICS INDEX</title>
-</head>
+@section('page-title', 'Comics')
 
-<body>
+@section('main')
+   <div class="container">
+      <div class="row">
 
-</body>
-
-</html>
+      </div>
+   </div>
+@endsection
