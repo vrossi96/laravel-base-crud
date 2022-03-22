9e22fb5da3b7582874dbb8ee117e81488570c476
vrossi96 <vrossi396@yahoo.it>
2022-03-22 17:46:15 +0100

    add navbar menu

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,23 @@
 <header>
-   <nav>
-      NAV
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+         <a class="navbar-brand" href="{{ route('comics.index') }}">Comics</a>
+         <button class="navbar-toggler" type="button">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link {{ request()->routeIs('comics.index') ? 'active' : '' }}"
+                     href="{{ route('comics.index') }}">Comics</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link {{ request()->routeIs('comics.create') ? 'active' : '' }}"
+                     href="{{ route('comics.create') }}">Aggiungi un
+                     Fumetto</a>
+               </li>
+            </ul>
+         </div>
+      </div>
    </nav>
 </header>
